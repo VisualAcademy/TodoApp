c8d16f171f7326ecbbb9bd8ebd432a40e552f675
RedPlus <redplus@redplus.net>
2020-08-12 17:01:57 +0900

    네트워크 프로그래밍 맛보기 데모

---
+++ TodoApp/TodoApp.Apis.Tests.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Apis.Tests.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string url = "https://localhost:44389/api/Todos";
+
+            using (var client = new HttpClient())
+            {
+                // 데이터 전송
+                var json = JsonConvert.SerializeObject(new Todo { Title = "HttpClient", IsDone = true });
+                var post = new StringContent(json, Encoding.UTF8, "application/json");
+                await client.PostAsync(url, post);
+                // 데이터 수신
+                var response = await client.GetAsync(url);
+                var result = await response.Content.ReadAsStringAsync();
+                var todos = JsonConvert.DeserializeObject<List<Todo>>(result);
+                foreach (var t in todos)
+                {
+                    Console.WriteLine($"{t.Id} - {t.Title}({t.IsDone})");
+                }
+            }
+        }
+    }
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ TodoApp/TodoApp.Apis/Controllers/TodosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Apis.Controllers
+{
+    [Route("api/[Controller]")]
+    public class TodosController : ControllerBase
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodosController()
+        {
+            _repository = new TodoRepositoryJson("C:\\temp\\Todos.json");
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //return Content("안녕하세요.");
+            return Ok(_repository.GetAll());
+        }
+        [HttpPost]
+        public IActionResult Add([FromBody]Todo dto)
+        {
+            _repository.Add(dto);
+            return Ok(dto);
+        }
+    }
+}
