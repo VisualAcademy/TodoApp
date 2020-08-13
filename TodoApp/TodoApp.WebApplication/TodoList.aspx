bd48a8acb8e243828ad6d02c8e9271ff7f0ec0d9
RedPlus <redplus@redplus.net>
2020-08-13 14:25:59 +0900

    LINQ로 함수형 프로그래밍 스타일 구현

---
+++ TodoApp/TodoApp.WebApplication/TodoList.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TodoList.aspx.cs" Inherits="TodoApp.WebApplication.TodoList" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>할 일 목록</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>할 일 목록</h1>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            <asp:GridView ID="GridView2" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TodoApp/TodoApp.WebApplication/TodoList.aspx.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace TodoApp.WebApplication
+{
+    public partial class TodoList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        private void DisplayData()
+        {
+            const string url = "https://localhost:44389/api/Todos";
+
+            using (var client = new HttpClient())
+            {
+                // 데이터 전송
+                var json = JsonConvert.SerializeObject(new Todo { Title = "HttpClient", IsDone = true });
+                var post = new StringContent(json, Encoding.UTF8, "application/json");
+                //client.PostAsync(url, post).Wait();
+                // 데이터 수신
+                var response = client.GetAsync(url).Result;
+                var result = response.Content.ReadAsStringAsync().Result;
+                var todos = JsonConvert.DeserializeObject<List<Todo>>(result);
+
+                // 필터링: LINQ로 함수형 프로그래밍 스타일 구현
+                // Select(): map()
+                //IEnumerable<Todo> q = from todo in todos
+                //        select todo;
+                //var q = todos.Select(t => t); 
+                var query = todos.AsQueryable<Todo>();
+                // 조건 처리
+                if (DateTime.Now.Second % 2 == 0)
+                {
+                    query = query.Where(it => it.Id % 2 == 0); // 짝수
+                }
+                else
+                {
+                    query = query.Where(it => it.Id % 2 == 1); // 홀수 
+                }
+                // 조건 처리
+                query = query.Where(it => it.IsDone == false);
+                // 정렬
+                const string sortOrder = "Title";
+                query = (sortOrder == "Title" ? query.OrderBy(it => it.Title) : query);
+                var q = query.Select(t => new TodoViewModel 
+                { 
+                    Title = t.Title, IsDone = t.IsDone 
+                });
+                // 데이터 바인딩
+                this.GridView1.DataSource = q;
+                this.GridView1.DataBind();
+
+                this.GridView2.DataSource = 
+                    todos
+                        .Where(t => t.Id % 2 == 1 && t.IsDone == false)
+                        .OrderByDescending(t => t.Title)
+                        .Select(t => new { 제목 = t.Title, 완료여부 = t.IsDone })
+                        .ToList();
+                this.GridView2.DataBind();
+            }
+        }
+    }
+    public class TodoViewModel
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
